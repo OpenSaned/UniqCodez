59c05117d3e36ce87734c71c334d8581980c4ea7
Ubuntu <kitta@ThatWeirdServer.1jwxr1dqovwelp1isppr5qi51g.phxx.internal.cloudapp.net>
2022-09-21 18:51:46 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Uniq Codez
+A Simple Way for people to leak their IPs online.
+
+This is an extremely light-weight and ***poorly written*** flask app, It works by getting the requester's IP and then "encoding" it to a "code" which then can be shared online to god knows how many people.
+
+If you got played by this Stupid Website then...
+
+
+**shut up and stop complaining.**
---
+++ index.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, render_template
+from time import time
+import os
+import requests
+
+app = Flask(__name__)
+def convertToBinary(int):
+    num = bin(int).replace("0b", "")
+    if len(num) < 8:
+        ext = "0" *(8 - len(num))
+        num = ext + num
+    return num.replace('1','▀').replace('0','▄')
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    ip = request.remote_addr 
+    with open("tweet.log", "a") as f:
+        f.write(f"{ip} - {time()}\n") 
+    return "200 OK"
+    
+@app.route('/')
+def index():
+    ip = request.remote_addr # change to ip = request.remote_addr for local hosting
+    code=[]
+    for i in ip.split('.'):
+        code.append(convertToBinary(int(i)))
+    with open("http.log", "a") as f:
+        f.write(f"{ip} - {time()}\n")
+    return render_template("index.html", code=code, host="uniqcodez.tk")
+        
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=80,debug=True)  
